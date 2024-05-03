0d1a79a387b25b53abe3e22b2861afddd48e7f98
Joël MAYEMBA <joelmayemba@gmail.com>
2024-05-03 11:26:05 +0200

    up

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --xformers --autolaunch --medvram --opt-sub-quad-attention --api --cors-allow-origins --lowvram --precision full --no-half --skip-torch-cuda-test
+set COMMANDLINE_ARGS= --xformers --autolaunch --medvram --opt-sub-quad-attention --api --cors-allow-origins --lowvram --precision full --no-half --skip-torch-cuda-test 
 
 call webui.bat
