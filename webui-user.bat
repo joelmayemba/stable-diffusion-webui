2058bbca4964964adcb3947ff035a3c9620a1f7c
Joël MAYEMBA <joelmayemba@gmail.com>
2024-05-20 17:58:42 +0200

    up

--- webui-user.bat
+++ webui-user.bat
@@ -3,8 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS = --xformers --lowvram --precision full --no-half --skip-torch-cuda-test
-
-
+set COMMANDLINE_ARGS = --xformers --medvram --precision full --no-half --skip-torch-cuda-test
 
 call webui.bat
