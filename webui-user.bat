00ec1765e95e61c357fa7896f9ad85ee04131214
Joël MAYEMBA <joelmayemba@gmail.com>
2025-07-21 14:36:05 +0200

    test

--- webui-user.bat
+++ webui-user.bat
@@ -3,7 +3,9 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS = --xformers --medvram --precision full --no-half --skip-torch-cuda-test --opt-sdp-no-mem-attention --no-half-vae 
+set COMMANDLINE_ARGS = --xformers --medvram --precision full --no-half --skip-torch-cuda-test --opt-sdp-no-mem-attention --no-half-vae --opt-split-attention
 @REM set COMMANDLINE_ARGS = --xformers
+ECHO Cleaning temp folder  
+DEL %temp%\*.png
 
 call webui.bat
